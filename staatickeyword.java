a5e92946f52bffcf2074d57e26c1149547bd0594
prakharmindfire <prakhar.mindfire@gmail.com>
2024-06-28 14:11:16 +0530

    Static keyword

---
+++ staatickeyword.java
@@ -0,0 +1,9 @@
+public class staatickeyword{
+    static void m1(){
+        System.out.println("Example of static keyword");
+    }
+
+    public static void main(String[] args) {
+        m1();
+    }
+}
